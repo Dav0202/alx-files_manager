bcaf28e08688a63bcd85e19c9b8c2df66abdfd22
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-05-25 14:23:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
+import express from 'express';
+import controllerRouting from './routes/index';
 
+/**
+ * The objective was to build a simple platform to upload and view files:
+ */
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+controllerRouting(app);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+export default app;
