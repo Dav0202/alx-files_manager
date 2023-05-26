9fbded380ca5734c7b9c34b469b8290887c2c63a
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-05-26 16:31:03 +0100

    Update basic.js

--- utils/basic.js
+++ utils/basic.js
@@ -1,1 +1,22 @@
+import { ObjectId } from 'mongodb';
 
+/**
+ * Module with basic utilities
+ */
+const basicUtils = {
+  /**
+   * Checks if Id is Valid for Mongo
+   * @id {string|number}
+   * @return {boolean}
+   */
+  isValidId(id) {
+    try {
+      ObjectId(id);
+    } catch (err) {
+      return false;
+    }
+    return true;
+  },
+};
+
+export default basicUtils;
